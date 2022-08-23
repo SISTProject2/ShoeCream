d92e088aba3d87a8e60905b0a6a3ea0480a98926
nmsgust <nmsgust@gmail.com>
2022-08-23 21:52:06 +0900

    충돌 수중 중

--- src/main/java/com/sist/vo/UserVO.java
+++ src/main/java/com/sist/vo/UserVO.java
@@ -1,31 +1,27 @@
 package com.sist.vo;
 
-import java.util.Date;
-
 /*
 
-USER_ID	NUMBER	No						1	시퀀스넘버
-EMAIL	VARCHAR2(100 BYTE)	No			2	이메일
-PWD	VARCHAR2(70 BYTE)	No				3	비밀번호
-NAME	VARCHAR2(20 BYTE)	No			4	이름
-TEL	VARCHAR2(50 BYTE)	No				5	핸드폰번호
-BIRTHDAY	DATE	Yes					6	생년월일
-GENDER	VARCHAR2(20 BYTE)	Yes			7	성별
-ZIPCODE	VARCHAR2(100 BYTE)	Yes			8	우편번호
-ADRESS	VARCHAR2(500 BYTE)	Yes			9	주소
-S_SIZE	VARCHAR2(20 BYTE)	Yes			10	사이즈
-IMG	VARCHAR2(2000 BYTE)	Yes				11	프로필 사진
-BANKNAME	VARCHAR2(50 BYTE)	Yes		12	은행사
-ACCNO	VARCHAR2(500 BYTE)	Yes			13	계좌번호
-ACCHOLDER	VARCHAR2(20 BYTE)	Yes		14	예금주
+USER_id	NUMBER				No			1	시퀀스넘버
+EMAIL		VARCHAR2(100 BYTE)	No			2	이메일
+PWD			VARCHAR2(70 BYTE)	No			3	비밀번호
+NAME		VARCHAR2(20 BYTE)	No			4	이름
+TEL			VARCHAR2(50 BYTE)	No			5	핸드폰번호
+BIRTHDAY	DATE				Yes			6	생년월일
+GENDER		VARCHAR2(20 BYTE)	Yes			7	성별
+ZIPCODE		VARCHAR2(100 BYTE)	Yes			8	우편번호
+ADRESS		VARCHAR2(500 BYTE)	Yes			9	주소
+S_SIZE		VARCHAR2(20 BYTE)	Yes			10	사이즈
+IMG			VARCHAR2(2000 BYTE)	Yes			11	프로필 사진
+BANKNAME	VARCHAR2(50 BYTE)	Yes			12	은행사
+ACCNO		VARCHAR2(500 BYTE)	Yes			13	계좌번호
+ACCHOLDER	VARCHAR2(20 BYTE)	Yes			14	예금주
 
  */
 public class UserVO {
 	
 	private int user_id;
-	private String email, pwd, name, tel, gender, zipcode, adress, s_size, img, bankname, accno, accholder ;
-	private Date birthday;
-	
+	private String email, pwd, name, tel, birthday, gender, zipcode, adress, s_size, img, bankname, accno, accholder, msg ;
 	public int getUser_id() {
 		return user_id;
 	}
@@ -55,6 +51,12 @@ public class UserVO {
 	}
 	public void setTel(String tel) {
 		this.tel = tel;
+	}
+	public String getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
 	}
 	public String getGender() {
 		return gender;
@@ -104,11 +106,12 @@ public class UserVO {
 	public void setAccholder(String accholder) {
 		this.accholder = accholder;
 	}
-	public Date getBirthday() {
-		return birthday;
+	public String getMsg() {
+		return msg;
 	}
-	public void setBirthday(Date birthday) {
-		this.birthday = birthday;
+	public void setMsg(String msg) {
+		this.msg = msg;
 	}
+	
 	
 }
