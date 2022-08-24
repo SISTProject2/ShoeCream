321be1691fba81c2c10997dcca341a8255f828b1
nmsgust <nmsgust@gmail.com>
2022-08-24 09:30:35 +0900

    pull수정

--- src/main/java/com/sist/vo/UserVO.java
+++ src/main/java/com/sist/vo/UserVO.java
@@ -19,99 +19,200 @@ ACCHOLDER	VARCHAR2(20 BYTE)	Yes			14	예금주
 
  */
 public class UserVO {
-	
+
 	private int user_id;
-	private String email, pwd, name, tel, birthday, gender, zipcode, adress, s_size, img, bankname, accno, accholder, msg ;
+	private String email, pwd, name, tel, gender, zipcode, adress, s_size, img, bankname, accno, accholder, birthday, msg ;
+
+
 	public int getUser_id() {
 		return user_id;
 	}
+
+
+
 	public void setUser_id(int user_id) {
 		this.user_id = user_id;
 	}
+
+
+
 	public String getEmail() {
 		return email;
 	}
+
+
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
+
+
 	public String getPwd() {
 		return pwd;
 	}
+
+
+
 	public void setPwd(String pwd) {
 		this.pwd = pwd;
 	}
+
+
+
 	public String getName() {
 		return name;
 	}
+
+
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
+
+
 	public String getTel() {
 		return tel;
 	}
+
+
+
 	public void setTel(String tel) {
 		this.tel = tel;
 	}
+
+
+
+	public String getGender() {
+		return gender;
+	}
+
+
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+
+
+	public String getZipcode() {
+		return zipcode;
+	}
+
+
+
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
+
+
+
+	public String getAdress() {
+		return adress;
+	}
+
+
+
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
+
+
+
+	public String getS_size() {
+		return s_size;
+	}
+
+
+
+	public void setS_size(String s_size) {
+		this.s_size = s_size;
+	}
+
+
+
+	public String getImg() {
+		return img;
+	}
+
+
+
+	public void setImg(String img) {
+		this.img = img;
+	}
+
+
+
+	public String getBankname() {
+		return bankname;
+	}
+
+
+
+	public void setBankname(String bankname) {
+		this.bankname = bankname;
+	}
+
+
+
+	public String getAccno() {
+		return accno;
+	}
+
+
+
+	public void setAccno(String accno) {
+		this.accno = accno;
+	}
+
+
+
+	public String getAccholder() {
+		return accholder;
+	}
+
+
+
+	public void setAccholder(String accholder) {
+		this.accholder = accholder;
+	}
+
+
+
 	public String getBirthday() {
 		return birthday;
 	}
 	public void setBirthday(String birthday) {
 		this.birthday = birthday;
 	}
-	public String getGender() {
-		return gender;
-	}
-	public void setGender(String gender) {
-		this.gender = gender;
-	}
-	public String getZipcode() {
-		return zipcode;
-	}
-	public void setZipcode(String zipcode) {
-		this.zipcode = zipcode;
-	}
-	public String getAdress() {
-		return adress;
-	}
-	public void setAdress(String adress) {
-		this.adress = adress;
-	}
-	public String getS_size() {
-		return s_size;
-	}
-	public void setS_size(String s_size) {
-		this.s_size = s_size;
-	}
-	public String getImg() {
-		return img;
-	}
-	public void setImg(String img) {
-		this.img = img;
-	}
-	public String getBankname() {
-		return bankname;
-	}
-	public void setBankname(String bankname) {
-		this.bankname = bankname;
-	}
-	public String getAccno() {
-		return accno;
-	}
-	public void setAccno(String accno) {
-		this.accno = accno;
-	}
-	public String getAccholder() {
-		return accholder;
-	}
-	public void setAccholder(String accholder) {
-		this.accholder = accholder;
-	}
+	
+	
 	public String getMsg() {
 		return msg;
 	}
 	public void setMsg(String msg) {
 		this.msg = msg;
 	}
-	
-	
+
+
+	@Override
+	public String toString() {
+		return "UserVO{" +
+				"user_id=" + user_id +
+				", email='" + email + '\'' +
+				", pwd='" + pwd + '\'' +
+				", name='" + name + '\'' +
+				", tel='" + tel + '\'' +
+				", gender='" + gender + '\'' +
+				", zipcode='" + zipcode + '\'' +
+				", adress='" + adress + '\'' +
+				", s_size='" + s_size + '\'' +
+				", img='" + img + '\'' +
+				", bankname='" + bankname + '\'' +
+				", accno='" + accno + '\'' +
+				", accholder='" + accholder + '\'' +
+				", birthday=" + birthday +
+				'}';
+	}
 }
